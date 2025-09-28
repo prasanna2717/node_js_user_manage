db4d263eaee6a5714d56977f06934b44d9e6c98c
prasanna2717 <prasannakumarr022002@gmail.com>
2025-09-28 19:35:00 +0530

    updates

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const { JWT_SECRET } = require("../security");
+const jwt = require('jsonwebtoken')
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "Token is missing" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid token", error: err.message });
+    }
+};
+
+module.exports = { authMiddleware }
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllUsers,loginUser,createUser,updateUser, deleteUser} =require("../controllers/authcontroller")
+const { getAllUsers, loginUser, createUser, updateUser, deleteUser } = require("../controllers/authcontroller");
+const { authMiddleware } = require("../middleware/authMiddleware");
 // Register new user (optional for testing)
 
 // Login
 router.post("/login", loginUser);
-router.get("/getuserlist",getAllUsers)
-router.post("/createUser",createUser)
-router.post("/updateuser/:id",updateUser)
+router.get("/getuserlist", authMiddleware, getAllUsers)
+router.post("/createUser", authMiddleware, createUser)
+router.post("/updateuser/:id", authMiddleware, updateUser)
 
-router.put("/updateuser/:id", updateUser);
-router.delete("/deleteuser/:id", deleteUser);
+router.put("/updateuser/:id", authMiddleware, updateUser);
+router.delete("/deleteuser/:id", authMiddleware, deleteUser);
 module.exports = router;
