eeffa11fda3ab0364951f35cdd3a4901c70485d3
prasanna2717 <prasannakumarr022002@gmail.com>
2025-09-28 18:29:36 +0530

    updates

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use(cors({
 // Routes
 app.use("/api/auth", authRoutes);
 
-const port = PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
     
